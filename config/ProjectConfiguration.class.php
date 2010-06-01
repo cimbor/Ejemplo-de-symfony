d47c4e5c0bc2d2177fd3d0990142c4f9473dc0a0
Marco Antonio Martin Callejo <marcomoguer@yahoo.es>
2010-06-01 02:36:05 +0200

    commit de mi primer ejemplo hecho realmente en netbeans

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/home/marco/symfony-1.4.4/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
